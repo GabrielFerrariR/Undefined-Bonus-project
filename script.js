15709b280789902f01f5b2573d1408cfcb067ab5
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2022-02-12 22:15:40 -0300

    Adiciona as respostas dinamicamente

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ const numOfCharacters = 826;
 const numOfLocations = 126;
 const numOfEpisodes = 51;
 const questionElement = document.getElementById('question-text');
-const answerElement = document.getElementsByClassName('answer');
 const charImgElement = document.querySelector('.img-container');
-
+const ansContainer = document.querySelector('.answers-container')
+// Gera a imagem do personagem
 const generateImg = (tag, url) => {
   const element = document.createElement(tag);
   element.src = url
   return element
 }
+// cria elementos variados a partir dos parametros dados
+const createElement = (element, className, innerText) => {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
+// Gera um número aleatório que será usado para chamar a API
 const generateRandomNumber = (maxNum) => {
   const min = 1;
   const max = maxNum;
@@ -19,18 +27,18 @@ const generateRandomNumber = (maxNum) => {
 }
 /* Randomize array in-place using Durstenfeld shuffle algorithm */
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+// Embaralha um array que será usado para adicionar as respostas em ordem aleatória
 const arrayShufle = () => {
   const array = [0,1,2,3]
-  function shuffleArray(array) {
   for (let index = array.length - 1; index > 0; index--) {
       let j = Math.floor(Math.random() * (index + 1));
       let temp = array[index];
       array[index] = array[j];
       array[j] = temp;
     }
-  }
   return array;
 }
+// função que busca na API
 const fetchData = async (data, id) => {
   const url = `${urlBase}${data}/${id}`;
   console.log(url);
@@ -53,26 +61,55 @@ const fetchData3Param = async (data, id, id2, id3, id4) => {
     return `Algo deu errado :( \n${error}`;
   }
 }
+// Gera a pergunta com base em um personagem aleatório
+let firstEp;
 const generateQuestion = async () => {
   const data = await fetchData('character', generateRandomNumber(826));
   const {name, image, episode} = data;
-  const firstEp = episode[0].match(/episode\/(.*)/)[1];
-  console.log(data,'outro console', name, image, firstEp);
+  firstEp = episode[0].match(/episode\/(.*)/)[1];
   questionElement.innerText = `O personagem ${name} apareceu pela primeira vez no episódio:`;
   const img = generateImg('img', image);
   charImgElement.appendChild(img);
   await generateAnswers(firstEp)
 }
+// Gera a span com as respostas 
+const generateSpan = async (array, index, id, name, episode) => {
+  const answerElement = document.querySelectorAll('.answer');
+  answerElement[array[index]].innerText = `${name} - ${episode}`
+  const span = createElement('span', 'ep_number', id);
+  answerElement[array[index]].appendChild(span);
+  answerElement.forEach((element) => {
+    element.addEventListener('click', episodeVerify)
+  })
+}
+// Gera as divs do answer-container dinamicamente
+const generateDivs = () => {
+  for (let index = 0; index < 4; index +=1) {
+    const div = createElement('div', 'answer');
+    ansContainer.appendChild(div);
+  }
+}
+// Função responsavel por chamar as respostas
 const generateAnswers = async (rightAnswer) => {
   const data = await fetchData3Param('episode', generateRandomNumber(numOfEpisodes), generateRandomNumber(numOfEpisodes), generateRandomNumber(numOfEpisodes), rightAnswer);
   const array = arrayShufle();
+  generateDivs()
+  const answerElement = document.querySelectorAll('.answer');
   data.forEach((ep, index) => {
-    const {name, episode} = ep
-    answerElement[array[index]].innerText = `${name} - ${episode}`;
+    const {name, episode, id} = ep
+    generateSpan(array, index, id, name, episode);
   })
-}  
+}
+// função que verifica se a resposta está certa ou errada
+const episodeVerify = (event) => {
+  const targetEp = event.target.lastChild.innerText;
+  console.log(targetEp);
+  (targetEp === firstEp)? event.target.classList.add('right_answer')
+  :event.target.classList.add('wrong_answer')
+}
+
 window.onload = async () => {
-  fetchData('character', 1).then((data) => console.log(data));
+  // fetchData('character', 1).then((data) => console.log(data));
   await generateQuestion();
   
 }
